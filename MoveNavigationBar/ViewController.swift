407838cefa64b7eaa2dee612bf2ddb4aa3098c00
veerareddygvr <verareddy007@gmail.com>
2018-03-13 13:15:44 +0530

    Initial Commit

---
+++ MoveNavigationBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoveNavigationBar
+//
+//  Created by Vera on 22/12/1939 Saka.
+//  Copyright © 1939 Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
